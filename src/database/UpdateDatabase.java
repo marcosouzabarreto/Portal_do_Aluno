e61244fa85dd7c30459297824f79071a619f1936
Marco Barreto <marcosouzabarreto@gmail.com>
2021-12-04 02:31:19 -0300

    crud almost done

---
+++ src/database/UpdateDatabase.java
@@ -0,0 +1,10 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UpdateDatabase {
+    private static final String SELECTION_QUERY = "update users set";
+}
